f3f29de06ae97efc21dda8124dbe4b35db670011
Douglas Silva <silvatec.br@gmail.com>
2022-07-22 11:00:47 -0300

    excluindo aluno

--- commands/remover-aluno.php
+++ commands/remover-aluno.php
@@ -8,11 +8,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $entityManagerFactory = new EntityManagerFactory();
 $entityManager = $entityManagerFactory->getEntityManager();
 
-$alunoRepository = $entityManager->getRepository(Aluno::class);
+$id = $argv[1];
+$aluno = $entityManager->getReference(Aluno::class, $id);
 
-/** @var Aluno[] $alunoList */
-$alunoList = $alunoRepository->findAll();
-
-foreach ($alunoList as $aluno) {
-    echo "ID: {$aluno->getId()}\nNome: {$aluno->getNome()}\n\n";
-}
+$entityManager->remove($aluno);
+$entityManager->flush();
